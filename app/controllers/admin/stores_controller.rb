02e10590e5ec090911ae8a4cbe223b5bd9b121e1
craig <craigya89@gmail.com>
2020-02-21 21:02:54 +0800

    Store CRUD - Add and modify admin/stores & stores view - Modify controller admin/stores & stores logic - Add and modify shared _flash, _navbar and latouts backend

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -1,2 +1,55 @@
 class Admin::StoresController < ApplicationController
+  before_action :find_store, only: [:show, :edit, :update, :destroy]
+
+  layout 'backend'
+  
+  def index
+    @stores = current_user.stores
+  end
+
+  def show
+  end
+
+  def new
+    @store = Store.new
+  end
+
+  def create
+    @store = Store.new(params_store)
+    if @store.save
+      redirect_to admin_stores_path, notice: '新增商店成功'
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @store.update(params_store)
+      redirect_to edit_admin_store_path, notice: '編輯商店成功'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @store.destroy
+    redirect_to admin_stores_path, notice: '刪除商店成功'
+  end
+
+  private
+
+  def find_store
+    @store = Store.find(params[:id])
+  end
+
+  def params_store
+    params.require(:store).permit(:name,
+                                  :note,
+                                  :tel,
+                                  :address,
+                                  :user_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # relationships
+  has_many :stores
+
+  def username
+    email.split('@')[0].capitalize
+  end
 end
