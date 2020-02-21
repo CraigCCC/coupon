9005cc90c537d7baec9d97f6d09caf8a2d7f0691
craig <craigya89@gmail.com>
2020-02-21 20:17:22 +0800

    Generate admin/stores, stores controller and Store model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'stores#index'
+  resources :stores, only: [:index, :show]
+
+  namespace :admin do
+    root 'stores#index'
+    resources :stores
+  end
 end
